1f52143cf2d2b972034cc3674537e24f2f1dd157
Oumnya benhassou <oumnya@users.noreply.github.com>
2023-06-30 03:22:58 +0400

    tt

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,7 +1,7 @@
 export const AppConfig = {
   site_name: 'Astro boilerplate testing auto deploy',
   title: 'Astro boilerplate',
-  description: 'Boilerplate built with Astro using React and Tailwind CSS',
+  description: 'Boilerplate built with Astro using React and Tailwind CSS test',
   author: 'Emma',
   locale_region: 'en-us',
   locale: 'en',
